3444545562b45b5159dd982f544744c089dc2c3d
Ignacio Floristan <38979090+ignaciofls@users.noreply.github.com>
2022-03-31 15:09:27 +0400

    Enhanced to handle multiple docs/records within one call

--- latestocr/__init__.py
+++ latestocr/__init__.py
@@ -12,7 +12,7 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Invoked OCR Skill')
     try:
         body = json.dumps(req.get_json())
-
+        logging.info("body: " + body)
         if body:
             #logging.info("Body is :", body)
             result = compose_response(body)
@@ -30,15 +30,15 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         )
 
 def compose_response(json_data):
-    values = json.loads(json_data)['values']    
+    values = json.loads(json_data)['values']
     # Prepare the Output before the loop
     results = {}
     results["values"] = []
     endpoint = os.environ["READ_ENDPOINT"]
-    key = os.environ["READ_ENDPOINT_KEY"]    
+    key = os.environ["READ_ENDPOINT_KEY"]
+    i=0
     for value in values:
         output_record = read(endpoint=endpoint, key=key, recordId=value["recordId"], data=value["data"])
-        logging.info("output_record: " + json.dumps(output_record, ensure_ascii=False))
         merged_content=""
         #navigate the json with a for loop and extract text field
         if 'data' in output_record:
@@ -47,8 +47,9 @@ def compose_response(json_data):
                     for l in i['lines']:
                         if 'text' in l:
                             merged_content=merged_content+l['text']+' '
-    output = {"values": [{"recordId": "0","data": {"text": merged_content}}]}
-    return json.dumps(output, ensure_ascii=False)
+        output = {"recordId": value["recordId"],"data": {"text": merged_content}}
+        results["values"].append(output)
+    return json.dumps(results, ensure_ascii=False)
 
 def read(endpoint, key, recordId, data):
     try:
@@ -60,20 +61,21 @@ def read(endpoint, key, recordId, data):
         #Read API works in two steps, first you post the job, afterwards you get the result
         response_job = requests.post(endpoint, data = body_json, headers = header)
         logging.info('Wait for Read API to process the document')
-        time.sleep(5) # Let some time to process the job, you could do active polling 
+        time.sleep(3) # Let some time to process the job, you could do active polling 
         urltoretrieveresult = response_job.headers["operation-location"]
         response = requests.get(urltoretrieveresult, None, headers=header)
         dict=json.loads(response.text)
-        #sometimes the Read processing time will be longer, in that case we need to try again after a while. You can probably add a more sophisticated retry policy here
-        if (dict['status']!='suceeded'):
-            logging.info('Read API processing time is longer than expected, retrying...')
-            time.sleep(10)
+        i=0
+        #sometimes the Read processing time will be longer, in that case we need to try again after a while
+        while dict["status"] != "Succeeded" and i<10:
+            time.sleep(3)
             response = requests.get(urltoretrieveresult, None, headers=header)
-            dict=json.loads(response.text)    
-        else:
-            logging.info('Timeout')
             dict=json.loads(response.text)
+            logging.info('Read API processing time is longer than expected, retrying...')
+            time.sleep(3)
+            i=i+1
         read_result=dict['analyzeResult']['readResults']
+        logging.info("Inside reading ")
         output_record = {
             "recordId": recordId,
             "data": read_result
@@ -84,5 +86,5 @@ def read(endpoint, key, recordId, data):
             "recordId": recordId,
             "errors": [ { "message": "Error: " + str(error) }   ] 
         }
-    logging.info("Output record: " + json.dumps(output_record, ensure_ascii=False))
+    #logging.info("Output record: " + json.dumps(output_record, ensure_ascii=False))
     return output_record
